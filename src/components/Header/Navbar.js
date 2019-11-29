531cbfd5b396dae7f206ba7f0fa312046dc74ce0
Negi47 <narendra.negi47@gmail.com>
2019-11-29 20:58:53 +0530

    header part

---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./navbar.css";
+import Logo from "../../assets/logo.png";
+import Profile from "../../assets/profile.png";
+
+function Navbar() {
+    return (
+        <nav className="header">
+            <div className="nav_wrapper">
+                <div className="logo_wrapper">
+                    <img src={Logo} />
+                </div>
+
+                <div className="search_wrapper">
+                    <input type="text" placeholder="Search" />
+                </div>
+
+                <div className="profileImageWrapper">
+                    <img src={Profile} />
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
